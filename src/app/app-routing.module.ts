0b5534150209161ce2f0362125ba9811daed5b56
nkunic <45877400+nkunic@users.noreply.github.com>
2023-07-18 17:31:57 +0200

    4 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomePageComponent } from './home-page/home-page.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const routes: Routes = [
-  { 
-    path: 'home-page', 
-    loadChildren: () => import('./home-page/home-page.module').then(m => m.HomePageModule) 
-  },
-  { 
-    path: 'page-not-found', 
-    loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule) 
-  }
-];
-
 const appRoutes: Routes = [
   {
     path: '',
     redirectTo: 'home-page',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'home-page',
-    component: HomePageComponent
+    loadChildren: () =>
+      import('./home-page/home-page.module').then((m) => m.HomePageModule),
   },
   {
-    path: '**',
-    component: PageNotFoundComponent,
-    pathMatch: 'full'
+    path: 'page-not-found',
+    loadChildren: () =>
+      import('./page-not-found/page-not-found.module').then(
+        (m) => m.PageNotFoundModule
+      ),
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
