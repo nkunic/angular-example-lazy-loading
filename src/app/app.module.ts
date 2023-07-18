23fc9332ddcc858bad5d6214ce664db9024b1739
nkunic <45877400+nkunic@users.noreply.github.com>
2023-07-18 17:45:46 +0200

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { HomePageModule } from './home-page/home-page.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule],
-  declarations: [AppComponent, HomePageComponent, PageNotFoundComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomePageModule,
+    PageNotFoundModule,
+  ],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
